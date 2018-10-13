f760bbb535921d32f2989d86089073b1d0cb4798
William Escobar <escow@farmingdale.edu>
2018-10-13 16:32:14 -0400

    2

--- login.php
+++ login.php
@@ -1,8 +1,41 @@
-# Group4
-Senior Project
-
 <!DOCTYPE html>
 <html>
-<head>
-  <title>Login Page</title>
-  <link
+	<head>
+		<title>My First php login page</title>
+		<link rel="stylesheet" type="text/css" href="style.css"></link>
+	</head>
+
+	<body>
+		<div class="container">
+			<img src="men.png" />
+			<form method="post" >
+				<div class="font-input">
+					<input type="text" name="username" placeholder="Enter Username">
+				</div>
+				<div class="font-input">
+					<input type="password" name="password" placeholder="Enter Password">
+				</div>
+				<input type="submit" name="submit" value="Login" class="btn-login"><br>
+				<a href="#"><p>forget password?</p></a>
+			</form>
+		</div>
+	</body>
+</html>
+
+
+<?php
+
+mysql_connect("localhost","root","");
+mysql_select_db('login');
+
+if(isset($_POST['submit'])){
+	$user = $_POST['username'];
+	$psd = $_POST['password'];
+
+	$query = "insert into loginpage (user_name,psdd) values ('$user','$psd')";
+
+	if(mysql_query($query)){
+
+	}
+}
+?>
